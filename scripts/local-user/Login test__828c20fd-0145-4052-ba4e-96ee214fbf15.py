4cb9d835874a55d1e6778801e1c5088b51ddaff0
Sai Santosh Pradyumna <saisantosh.pradyumna@protestcorp.com>
2026-03-27 14:41:04 +0530

    Change username in login test to 'Wronguserid'

--- scripts/local-user/Login test__828c20fd-0145-4052-ba4e-96ee214fbf15.py
+++ scripts/local-user/Login test__828c20fd-0145-4052-ba4e-96ee214fbf15.py
@@ -34,7 +34,7 @@ def run(playwright: Playwright) -> None:
     try:
         page.goto("https://www.saucedemo.com/")
         page.locator("[data-test=\"username\"]").click()
-        page.locator("[data-test=\"username\"]").fill("standard_user")
+        page.locator("[data-test=\"username\"]").fill("Wronguserid")
         page.locator("[data-test=\"password\"]").click()
         page.locator("[data-test=\"password\"]").fill("secret_sauce")
         page.locator("[data-test=\"login-button\"]").click()
